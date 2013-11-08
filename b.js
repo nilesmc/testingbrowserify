3d52d7df744a8be78b012d40cbb6f4d571609291
garbagemanonfire <nilesvm@gmail.com>
2013-11-07 19:27:13 -0800

    Browserify and beefy are sweet

---
+++ b.js
@@ -0,0 +1,10 @@
+var c = require('./c');
+
+// c(); // print Pooop c
+
+// module.exports = function() {
+//     console.log("Poop b");
+// };
+
+console.log(c);
+
---
+++ c.js
@@ -0,0 +1,10 @@
+var object2 = {
+    name: "test",
+    othername: "beta",
+};
+
+module.exports = object2.name;
+
+// function() {
+//     console.log("Poop C");
+// };
